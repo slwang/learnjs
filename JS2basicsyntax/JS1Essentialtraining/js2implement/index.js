8e4be575a2c5baf8d4a0c08d1b5f6063e0da830a
wsl <wsl@chanjet.com>
2016-03-17 16:53:23 +0800

    add code

---
+++ JS2basicsyntax/JS1Essentialtraining/js2implement/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by wsl on 2016/3/17.
+ */
+document.write("hello js");
---
+++ JS2basicsyntax/JS1Essentialtraining/js5syntax/comments.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/3/17.
+ */
+document.write("1");//打印输出
+/*
+document.write("2");
+document.write("3");
+document.write("4");
+*/
+
